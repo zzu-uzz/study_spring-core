2e6c2059e951b7a7b0db02b106eaaf9d010643ee
oyeonjoo <oyj0842@naver.com>
2024-05-25 18:31:21 +0900

    :sparkles: 회원 가입 테스트

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1l);
+
+        // then
+        Assertions.assertEquals(member, findMember);
+    }
+}
