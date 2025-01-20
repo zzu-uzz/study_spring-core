2645d7d46755eff86de30516ec6725ccc432006e
oyeonjoo <oyj0842@naver.com>
2025-01-20 19:14:08 +0900

    refactor: 어노테이션 직접 만들어서 @Qualifier 적용하기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -14,9 +15,9 @@ public class OrderServiceImpl implements OrderService {
   private final DiscountPolicy discountPolicy;
 
   @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+  public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
-    this.discountPolicy = rateDiscountPolicy;
+    this.discountPolicy = discountPolicy;
   }
 
   @Override
