7df395c1a3f2c6d347693549c7d3d3f06784db79
oyeonjoo <oyj0842@naver.com>
2025-01-21 20:06:22 +0900

    add: request 스코프와 Provider

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -18,7 +18,7 @@ public class MyLogger {
   }
 
   public void log(String message) {
-    System.out.println("[" + uuid + "]" + "[" + requestURL + "}" + message);
+    System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
   }
 
   @PostConstruct
